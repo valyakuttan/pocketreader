5349908f09bfe7a8b750b7efe5de8ebe536a0f6c
valyakuttan <valyakuttan@noreply.com>
2019-01-20 12:35:36 +0530

    Updates

---
+++ precache-manifest.04732964794d5a61f63b49a0e593c37d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "38f49a5f00fd2d80bee1",
+    "url": "/pocketreader/static/css/main.4f7c0a85.chunk.css"
+  },
+  {
+    "revision": "38f49a5f00fd2d80bee1",
+    "url": "/pocketreader/static/js/main.38f49a5f.chunk.js"
+  },
+  {
+    "revision": "71f1a2545e582d3d7565",
+    "url": "/pocketreader/static/js/1.71f1a254.chunk.js"
+  },
+  {
+    "revision": "618e4d44aedbfb5bab4d",
+    "url": "/pocketreader/static/js/runtime~main.618e4d44.js"
+  },
+  {
+    "revision": "72ffe8d4edf94119a14fc3713d6471b8",
+    "url": "/pocketreader/static/media/logo.72ffe8d4.png"
+  },
+  {
+    "revision": "4fe19aac180d2f5433769165680570a8",
+    "url": "/pocketreader/index.html"
+  }
+];
